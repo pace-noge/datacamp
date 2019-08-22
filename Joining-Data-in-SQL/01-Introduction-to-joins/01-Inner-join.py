340de1a23e40930615e5ee3f10ae0f77d6797094
Nasaruddin <nasa.freaks@gmail.com>
2019-08-22 16:38:49 +0700

    Update 01-Inner-join.py

--- Joining-Data-in-SQL/01-Introduction-to-joins/01-Inner-join.py
+++ Joining-Data-in-SQL/01-Introduction-to-joins/01-Inner-join.py
@@ -22,3 +22,21 @@ Select all column from cities
 """
 -- Select all columns from cities
 SELECT * FROM cities;
+
+
+# 2. 
+
+SELECT * 
+FROM cities
+  -- 1. Inner join to countries
+  INNER JOIN countries
+    -- 2. Match on the country codes
+    ON cities.country_code = country.code;
+    
+# 3. Select name fields (with alias) and region 
+
+
+SELECT cities.name AS city, countries.name AS country, countries.region
+FROM cities
+  INNER JOIN countries
+    ON cities.country_code = countries.code;
