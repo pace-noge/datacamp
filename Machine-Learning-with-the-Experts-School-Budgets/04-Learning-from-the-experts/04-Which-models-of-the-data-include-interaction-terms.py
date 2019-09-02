df276596f48bc938d4220c23807746a3b0ae9a11
Nasaruddin <nasa.freaks@gmail.com>
2019-09-02 13:20:50 +0700

    Create 04-Which-models-of-the-data-include-interaction-terms.py

---
+++ Machine-Learning-with-the-Experts-School-Budgets/04-Learning-from-the-experts/04-Which-models-of-the-data-include-interaction-terms.py
@@ -0,0 +1,31 @@
+"""
+Which models of the data include interaction terms?
+Recall from the video that interaction terms involve products of features.
+
+Suppose we have two features x and y, and we use models that process the features as follows:
+
+βx + βy + ββ
+βxy + βx + βy
+βx + βy + βx^2 + βy^2
+where β is a coefficient in your model (not a feature).
+
+Which expression(s) include interaction terms?
+
+INSTRUCTION
+-----------
+
+The first expression.
+press
+1
+The second expression.
+press
+2
+The third expression.
+press
+3
+The first and third expressions.
+press
+4
+"""
+
+The second expression.
