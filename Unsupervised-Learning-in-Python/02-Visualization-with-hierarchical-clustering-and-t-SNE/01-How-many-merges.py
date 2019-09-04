f9abba85e058d9f302eea41e8703ee7076e291cd
Nasaruddin <nasa.freaks@gmail.com>
2019-09-04 09:54:25 +0700

    Create 01-How-many-merges.py

---
+++ Unsupervised-Learning-in-Python/02-Visualization-with-hierarchical-clustering-and-t-SNE/01-How-many-merges.py
@@ -0,0 +1,19 @@
+"""
+
+How many merges?
+If there are 5 data samples, how many merge operations will occur in a hierarchical clustering? To help answer this question, think back to the video, in which Ben walked through an example of hierarchical clustering using 6 countries. How many merge operations did that example have?
+
+INSTRUCTION
+-----------
+4 merges.
+press
+1
+5 merges.
+press
+2
+This can't be known in advance.
+press
+
+"""
+
+# 4 merges
