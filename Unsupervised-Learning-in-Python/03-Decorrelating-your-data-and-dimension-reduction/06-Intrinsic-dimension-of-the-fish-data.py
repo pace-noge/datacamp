56f1846f5292b1149d587262f159b9008c3f1018
Nasaruddin <nasa.freaks@gmail.com>
2019-09-04 17:01:57 +0700

    Create 06-Intrinsic-dimension-of-the-fish-data.py

---
+++ Unsupervised-Learning-in-Python/03-Decorrelating-your-data-and-dimension-reduction/06-Intrinsic-dimension-of-the-fish-data.py
@@ -0,0 +1,14 @@
+"""
+Intrinsic dimension of the fish data
+In the previous exercise, you plotted the variance of the PCA features of the fish measurements. Looking again at your plot, what do you think would be a reasonable choice for the "intrinsic dimension" of the the fish measurements? Recall that the intrinsic dimension is the number of PCA features with significant variance.
+
+INSTRUCTION
+-----------
+
+Possible Answers
+1
+2
+5
+"""
+
+# 2
