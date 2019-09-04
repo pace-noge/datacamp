8158a10100422b8d2e81f6e80c0e3e5643a8bae3
Nasaruddin <nasa.freaks@gmail.com>
2019-09-04 10:38:56 +0700

    Create 04-Which-clusters-are-closest.py

---
+++ Unsupervised-Learning-in-Python/02-Visualization-with-hierarchical-clustering-and-t-SNE/04-Which-clusters-are-closest.py
@@ -0,0 +1,20 @@
+"""
+Which clusters are closest?
+In the video, you learned that the linkage method defines how the distance between clusters is measured. In complete linkage, the distance between clusters is the distance between the furthest points of the clusters. In single linkage, the distance between clusters is the distance between the closest points of the clusters.
+
+Consider the three clusters in the diagram. Which of the following statements are true?
+
+A. In single linkage, cluster 3 is the closest to cluster 2.
+
+B. In complete linkage, cluster 1 is the closest to cluster 2.
+
+INSTRUCTION
+-----------
+Possible Answers
+Neither A nor B.
+A only.
+Both A and B.
+
+"""
+
+#Both A and B.
