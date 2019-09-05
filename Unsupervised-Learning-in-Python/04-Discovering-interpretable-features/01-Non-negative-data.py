3e45f2035e8f57befedcb94f72ab6c65019cf046
Nasaruddin <nasa.freaks@gmail.com>
2019-09-05 11:46:13 +0700

    Create 01-Non-negative-data.py

---
+++ Unsupervised-Learning-in-Python/04-Discovering-interpretable-features/01-Non-negative-data.py
@@ -0,0 +1,23 @@
+"""
+Non-negative data
+Which of the following 2-dimensional arrays are examples of non-negative data?
+
+A tf-idf word-frequency array.
+An array daily stock market price movements (up and down), where each row represents a company.
+An array where rows are customers, columns are products and entries are 0 or 1, indicating whether a customer has purchased a product.
+
+INSTRUCTION
+-----------
+
+Possible Answers
+1 only
+press
+1
+2 and 3
+press
+2
+1 and 3
+press
+
+"""
+1 and 3
