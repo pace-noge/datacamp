86a8acb378b889a8669b6001d9569abe97aa9da7
Nasaruddin <nasa.freaks@gmail.com>
2019-09-06 09:11:50 +0700

    Create 04-Growing-a-classification-tree.py

---
+++ Machine-Learning-with-Tree-Based-Models-in-Python/01-Classification-and-Regression-Trees/04-Growing-a-classification-tree.py
@@ -0,0 +1,22 @@
+"""
+Growing a classification tree
+In the video, you saw that the growth of an unconstrained classification tree followed a few simple rules. Which of the following is not one of these rules?
+
+INSTRUCTION
+-----------
+
+Possible Answers
+The existence of a node depends on the state of its predecessors.
+press
+1
+The impurity of a node can be determined using different criteria such as entropy and the gini-index.
+press
+2
+When the information gain resulting from splitting a node is null, the node is declared as a leaf.
+press
+3
+When an internal node is split, the split is performed in such a way so that information gain is minimized.
+press
+
+"""
+# When an internal node is split, the split is performed in such a way so that information gain is minimized.
