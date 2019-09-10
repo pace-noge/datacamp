af71f0ef1ba8364d8d27d435b757b285a12fb7b0
Nasaruddin <nasa.freaks@gmail.com>
2019-09-10 17:38:49 +0700

    Create 10-A-round-of-backpropagation.py

---
+++ Deep-Learning-in-Python/02-Optimizing-a-neural-network-with-backward-propagation/10-A-round-of-backpropagation.py
@@ -0,0 +1,30 @@
+"""
+A round of backpropagation
+In the network shown below, we have done forward propagation, and node values calculated as part of forward propagation are shown in white. The weights are shown in black. Layers after the question mark show the slopes calculated as part of back-prop, rather than the forward-prop values. Those slope values are shown in purple.
+
+This network again uses the ReLU activation function, so the slope of the activation function is 1 for any node receiving a positive value as input. Assume the node being examined had a positive value (so the activation function's slope is 1).
+
+Ch2Ex14_1
+
+What is the slope needed to update the weight with the question mark?
+
+Ch2Ex14_2
+
+INSTRUCTION
+-----------
+
+
+0.
+press
+1
+2.
+press
+2
+6.
+press
+3
+Not enough information.
+press
+
+"""
+# 6.
