d3e0cbfe1344cdf3d799cd20a31eee26794f04bf
Nasaruddin <nasa.freaks@gmail.com>
2019-09-10 08:32:50 +0700

    Create 05-Forward-propagation-in-a-deeper-network.py

---
+++ Deep-Learning-in-Python/01-Basics-of-deep-learning-and-neural-networks/05-Forward-propagation-in-a-deeper-network.py
@@ -0,0 +1,24 @@
+"""
+Forward propagation in a deeper network
+You now have a model with 2 hidden layers. The values for an input data point are shown inside the input nodes. The weights are shown on the edges/lines. What prediction would this model make on this data point?
+
+Assume the activation function at each node is the identity function. That is, each node's output will be the same as its input. So the value of the bottom node in the first hidden layer is -1, and not 0, as it would be if the ReLU activation function was used.
+
+Ch1Ex9
+
+INSTRUCTION
+-----------
+
+Possible Answers
+0.
+press
+1
+7.
+press
+2
+9.
+press
+
+"""
+
+# 0
