447ac5fdf9cc62d0ab38cbfe7dc05bfbd8e716db
Nasaruddin <nasa.freaks@gmail.com>
2019-09-10 08:48:33 +0700

    Create 01-Calculating-model-errors.py

---
+++ Deep-Learning-in-Python/02-Optimizing-a-neural-network-with-backward-propagation/01-Calculating-model-errors.py
@@ -0,0 +1,25 @@
+"""
+Calculating model errors
+For the exercises in this chapter, you'll continue working with the network to predict transactions for a bank.
+
+What is the error (predicted - actual) for the following network when the input data is [3, 2] and the actual value of the target (what you are trying to predict) is 5? It may be helpful to get out a pen and piece of paper to calculate these values.
+
+Ch2Ex2
+
+INSTRUCTION
+------------
+Possible Answers
+5.
+press
+1
+6.
+press
+2
+11.
+press
+3
+16.
+press
+
+"""
+# 11
