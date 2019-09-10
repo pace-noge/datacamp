3057ad779d482adacf1dfe8769c93b233a140b29
Nasaruddin <nasa.freaks@gmail.com>
2019-09-10 07:57:33 +0700

    Create 01-Comparing-neural-network-models-to-classical-regression-models.py

---
+++ Deep-Learning-in-Python/01-Basics-of-deep-learning-and-neural-networks/01-Comparing-neural-network-models-to-classical-regression-models.py
@@ -0,0 +1,19 @@
+"""
+Which of the models in the diagrams has greater ability to account for interactions?
+
+INSTRUCTION
+-----------
+
+Possible Answers
+Model 1.
+press
+1
+Model 2.
+press
+2
+They are both the same.
+press
+
+"""
+
+# Model 2.
