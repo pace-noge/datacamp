9c8cb408f80d1b1c65853504a23ae456f582fb71
Nasaruddin <nasa.freaks@gmail.com>
2019-09-10 08:41:39 +0700

    Create 08-Levels-of-representation.py

---
+++ Deep-Learning-in-Python/01-Basics-of-deep-learning-and-neural-networks/08-Levels-of-representation.py
@@ -0,0 +1,19 @@
+"""
+Levels of representation
+Which layers of a model capture more complex or "higher level" interactions?
+
+INSTRUCTION
+-----------
+Possible Answers
+The first layers capture the most complex interactions.
+press
+1
+The last layers capture the most complex interactions.
+press
+2
+All layers capture interactions of similar complexity.
+press
+
+"""
+
+# he last layers capture the most complex interactions.
