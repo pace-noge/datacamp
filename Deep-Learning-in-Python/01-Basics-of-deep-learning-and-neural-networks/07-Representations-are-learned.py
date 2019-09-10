4066cb0a47a77dcbe92c671f7ecc852d35309d09
Nasaruddin <nasa.freaks@gmail.com>
2019-09-10 08:40:27 +0700

    Create 07-Representations-are-learned.py

---
+++ Deep-Learning-in-Python/01-Basics-of-deep-learning-and-neural-networks/07-Representations-are-learned.py
@@ -0,0 +1,18 @@
+"""
+Representations are learned
+How are the weights that determine the features/interactions in Neural Networks created?
+
+INSTRUCTION
+-----------
+A user chooses them when creating the model.
+press
+1
+The model training process sets them to optimize predictive accuracy.
+press
+2
+The weights are random numbers.
+press
+
+"""
+
+# The model training process sets them to optimize predictive accuracy.
