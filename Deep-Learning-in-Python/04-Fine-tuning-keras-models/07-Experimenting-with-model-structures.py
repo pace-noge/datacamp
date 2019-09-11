7cebb27113540feb4b1e2994889094aa891124c0
Nasaruddin <nasa.freaks@gmail.com>
2019-09-11 10:55:23 +0700

    Create 07-Experimenting-with-model-structures.py

---
+++ Deep-Learning-in-Python/04-Fine-tuning-keras-models/07-Experimenting-with-model-structures.py
@@ -0,0 +1,19 @@
+"""
+Experimenting with model structures
+You've just run an experiment where you compared two networks that were identical except that the 2nd network had an extra hidden layer. You see that this 2nd network (the deeper network) had better performance. Given that, which of the following would be a good experiment to run next for even better performance?
+
+INSTRUCTION
+-----------
+Possible Answers
+Try a new network with fewer layers than anything you have tried yet.
+press
+1
+Use more units in each hidden layer.
+press
+2
+Use fewer units in each hidden layer.
+press
+
+"""
+
+# se more units in each hidden layer.
