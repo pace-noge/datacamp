f9265612548b8ae5895a07302994541166fac8fc
Nasaruddin <nasa.freaks@gmail.com>
2019-09-11 10:09:38 +0700

    Create 01-Diagnosing-optimization-problems.py

---
+++ Deep-Learning-in-Python/04-Fine-tuning-keras-models/01-Diagnosing-optimization-problems.py
@@ -0,0 +1,21 @@
+"""
+Diagnosing optimization problems
+Which of the following could prevent a model from showing an improved loss in its first few epochs?
+
+INSTRUCTION
+-----------
+Possible Answers
+Learning rate too low.
+press
+1
+Learning rate too high.
+press
+2
+Poor choice of activation function.
+press
+3
+All of the above.
+press
+
+"""
+# All of the above.
