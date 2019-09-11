117c822391878c98356db316796ef140e4c6e50b
Nasaruddin <nasa.freaks@gmail.com>
2019-09-11 11:40:33 +0700

    Create 01-What-is-a-network.py

---
+++ Network-Analysis-in-Python-(Part-1)/01-Introduction-to-networks/01-What-is-a-network.py
@@ -0,0 +1,21 @@
+"""
+What is a network?
+Let's think again about examples of networks. Which of the following data is least easily modeled as a network?
+
+INSTRUCTION
+-----------
+Airplane transportation.
+press
+1
+Phone numbers in a telephone directory.
+press
+2
+Co-authorship of papers.
+press
+3
+Atoms in a molecule.
+press
+
+"""
+
+# Phone numbers in a telephone directory.
