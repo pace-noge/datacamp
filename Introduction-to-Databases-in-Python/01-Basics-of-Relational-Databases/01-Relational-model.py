f8edfb1c136a5ad2fb132d864696c3c67b111d0f
Nasaruddin <nasa.freaks@gmail.com>
2019-09-12 16:11:01 +0700

    Create 01-Relational-model.py

---
+++ Introduction-to-Databases-in-Python/01-Basics-of-Relational-Databases/01-Relational-model.py
@@ -0,0 +1,25 @@
+"""
+Relational model
+Which of the following is not part of the relational model?
+
+INSTRUCTION
+----------
+Possible Answers
+Tables
+press
+1
+Columns
+press
+2
+Rows
+press
+3
+Dimensions
+press
+4
+Relationships
+press
+
+"""
+
+# Dimensions
