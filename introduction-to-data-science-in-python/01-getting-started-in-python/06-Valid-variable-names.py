4170b51fd15c04ccb37b49dfba3c3c961b6f75e4
Nasaruddin <nasa.freaks@gmail.com>
2019-09-17 08:57:36 +0700

    Create 06-Valid-variable-names.py

---
+++ introduction-to-data-science-in-python/01-getting-started-in-python/06-Valid-variable-names.py
@@ -0,0 +1,14 @@
+"""
+Valid variable names
+Which of the following is not a valid variable name?
+
+INSTRUCTION
+-----------
+Possible Answers
+my_dog_bayes
+BAYES42
+3dogs
+this_is_a_very_long_variable_name_42
+"""
+
+# 3dogs
