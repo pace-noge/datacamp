bdfbdc6290410ab3ae33609faa1db7c4a3e5ae3a
Nasaruddin <nasa.freaks@gmail.com>
2019-09-17 10:35:06 +0700

    Create 02-Inspecting-a-DataFrame.py

---
+++ introduction-to-data-science-in-python/02-loading-data-in-pandas/02-Inspecting-a-DataFrame.py
@@ -0,0 +1,32 @@
+"""
+Inspecting a DataFrame
+We've loaded the credit card records of our four suspects into a DataFrame called credit_records. Let's learn more about the structure of this DataFrame.
+
+The pandas module has been imported under the alias pd. The DataFrame credit_records has already been imported.
+
+How many rows are in credit_records?
+
+INSTRUCTION
+-----------
+1/2
+Use the .info() method to inspect the DataFrame credit_records
+
+2/2
+
+Question
+How many rows are in credit_records?
+
+Possible Answers
+103
+104
+5
+64
+
+"""
+
+# 1/2
+# Use .info() to inspect the DataFrame credit_records
+print(credit_records.info())
+
+# 2/2
+104
